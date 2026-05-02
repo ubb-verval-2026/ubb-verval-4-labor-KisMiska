59e5be7ab02e571eb235d94d794bfc24e2e360a7
KisMiska <galicaervin@gmail.com>
2026-05-02 14:12:49 +0300

    2 - test for less than -10 percent

--- DatesAndStuff.Web/test/DatesAndStuff.Web.Tests/PersonPageTests.cs
+++ DatesAndStuff.Web/test/DatesAndStuff.Web.Tests/PersonPageTests.cs
@@ -126,6 +126,32 @@ public class PersonPageTests
         var salaryAfterSubmission = double.Parse(salaryLabel.Text);
         salaryAfterSubmission.Should().BeApproximately(expectedSalary, 0.001);
     }
+
+    [Test]
+    public void Person_SalaryIncrease_LessThanMinusTen_ShouldShowValidationMessagesInSummaryAndUnder()
+    {
+        // Arrange
+        driver.Navigate().GoToUrl(BaseURL);
+        driver.FindElement(By.XPath("//*[@data-test='PersonPageNavigation']")).Click();
+
+        var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+
+        var input = wait.Until(ExpectedConditions.ElementExists(By.XPath("//*[@data-test='SalaryIncreasePercentageInput']")));
+        input.Clear();
+        input.SendKeys("-10.01");
+
+        // Act
+        var submitButton = wait.Until(ExpectedConditions.ElementExists(By.XPath("//*[@data-test='SalaryIncreaseSubmitButton']")));
+        submitButton.Click();
+
+        // Assert
+        var summaryError = wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//ul[contains(@class,'validation-errors')]//li[contains(.,'between -10 and infinity')]")));
+        var fieldError = wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//*[contains(@class,'validation-message') and contains(.,'between -10 and infinity')]")));
+
+        summaryError.Displayed.Should().BeTrue();
+        fieldError.Displayed.Should().BeTrue();
+    }
+
     private bool IsElementPresent(By by)
     {
         try
